7e720d4cedcea18c41f480f0face6e653b05e77a
raghad6 <raghadsharar4@gmail.com>
2022-04-06 09:39:26 +0300

    firebase added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig : {
+    apiKey: "AIzaSyAR9EIQfE89zclh5YNsgV4bUJAMvQTxJpc",
+    authDomain: "website-dashboard-b68f9.firebaseapp.com",
+    projectId: "website-dashboard-b68f9",
+    storageBucket: "website-dashboard-b68f9.appspot.com",
+    messagingSenderId: "952081910544",
+    appId: "1:952081910544:web:3c2cb74bade0337cb6f395",
+    measurementId: "G-9YLMWXPX87"
+  }
 };
